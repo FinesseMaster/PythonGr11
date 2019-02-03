fb2e57393b08d35fdd39f19aefb773a05f21f665
Mike Kagnew <44201432+FinesseMaster@users.noreply.github.com>
2019-02-03 15:42:48 -0500

    Version 3 of tictactoe

--- TicTactoe/TicTacToe.py
+++ TicTactoe/TicTacToe.py
@@ -2,16 +2,71 @@
 TicTacToe
 Michael Kagnew and Sandra Fang
 
-Will output the winner immediately, still has no reset button"""
+NO First complete fail attempt at making a reset button, atrocious"""
+
 
 from tkinter import *
-from tkinter import messagebox 
+from tkinter import messagebox
+import sys
+import os
+messagebox.showinfo("Rules", "Rules are very straight forward, get 3 in a row diagonally, vertically, or horizontally to win!")
+##class Window(Frame):
+##    def __init__(self, master= None):
+##        Frame.__init__(self, master)
+##
+##        self.master=master
+##        self.init_window()
+##
+##    def init_window(self):
+##        self.master.title("GUI")
+##
+##        self.pack(fill=BOTH, expand=1)
+##
+##        startButton= Button(self, text="Start", command=self.client_start)
+##
+##        startButton.place(x=30, y=30)
+##
+##    def client_start(self):
+##        pass
+##        
+##
+##root = Tk()
+##w= Label(root, text="The Start")
+##w.pack
+##app=Window(root)
+
+
+
+
+
+
+##
+##def start(self):
+##     break
+##
+##
+##     bl=Button( text="Start", command=self)
+##     bl.pack()
+    
+    
+   
+
+   
+
+
+
+
+
+
+
+
 
 root= Tk()
 root.title("Michael And Sandula's tic tac toe")
 
 bclick= True
 counter = int(0)
+
 
 
 
@@ -55,6 +110,7 @@ def tictactoe(buttons):
     
 
 
+
 buttons=StringVar()
 button1=Button(root, text=" ", font=("Arial 35 bold"), height=4, width= 8, command=lambda:tictactoe(button1))
 button1.grid(row=1, column=0, sticky=W+E+N+S)
@@ -85,6 +141,18 @@ button9=Button(root, text=" ", font=("Arial 35 bold"), height=4, width= 8, comma
 button9.grid(row=3, column=2, sticky=W+E+N+S)
 
 
-
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+if __name__ == "__main__":
+    answer = input("Do you want to restart this program ? ")
+    if answer.lower().strip() in "y yes".split():
+        restart_program()
 root.mainloop()
+
+
+
 
